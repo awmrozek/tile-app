c1bb62dd6c66fb717db544c0a35f39c8504218f7
Adam Mrozek <mrozekss@gmail.com>
2025-09-17 08:51:48 +0200

    Added material toolbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,26 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import TileComponent from './TileComponent';
 import content from './content'; // Import the content
+import { AppBar, Toolbar, Typography, Button, Container, Grid, Card, CardContent, CardMedia } from '@mui/material';
 import './App.css';
 
 const App = () => {
     return (
-        <div className="app-container">
+            <div className="app-container">
+            <AppBar position="static">
+            <Toolbar>
+            <Typography variant="h6" style={{ flexGrow: 1 }}>
+            My short presentation app
+            </Typography>
+            <Button color="inherit">Login</Button>
+            </Toolbar>
+            </AppBar>
+
+            <div className="header">
+            
             <h1>Adam <b>Mrozek</b></h1>
             <center><i>Engineering</i></center>
+            </div>
             <div className="tiles-container">
                 {Object.keys(content).map(key => (
                     <TileComponent key={key} title={content[key].title} targetId={key} />
@@ -21,6 +34,8 @@ const App = () => {
                         <ReactMarkdown>{content[key].text}</ReactMarkdown>
                     </section>
                 ))}
+            </div>
+            <div className="footer">
             </div>
         </div>
     );
